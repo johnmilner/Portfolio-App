406fd761c8217bd8652cf9fceadec6c43debbb6f
John Milner <john.milner@mac.com>
2018-03-08 20:48:40 -0800

    rename scss folder to styles folder and update style gulp task to combine css files

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,20 +1,33 @@
 var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 sass    = require('gulp-sass'),
-autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins'),
-hexrgba = require('postcss-hexrgba');
+hexrgba = require('postcss-hexrgba'),
+concatcss = require('gulp-concat-css'),
+cssnext = require('postcss-cssnext');
+
 
 gulp.task('styles', function() {
-  return gulp.src(['./app/assets/scss/**/*.scss', './app/assets/css/**/*.css' ])
+  return gulp.src(['./app/assets/styles/**/*.scss', './app/assets/styles/vendor/**/*.css' ])
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+    .pipe(concatcss('main.css'))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, cssnext]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
     })
     .pipe(gulp.dest('./app/temp/styles'));
 });
+
+// return (
+//   //gulp.src('./src/css/**/*.scss')
+//   //.pipe(sass.sync().on('error', sass.logError))
+//   //.pipe(concatcss('all.css'))
+//   .pipe(postcss([
+//     cssNext()
+//   ]))
+//   .pipe(gulp.dest('./dist/css'))
+// )
