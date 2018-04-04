1f11618afc72aea96c32ba194a7f69446b736ece
John Milner <john.milner@mac.com>
2018-04-03 19:00:14 -0700

    add btn-liquid to js-scroll script

--- app/assets/scripts/modules/script.js
+++ app/assets/scripts/modules/script.js
@@ -464,7 +464,7 @@ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     var target = $(this.hash);
     target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
     if (target.length) {
-      $("html, body").animate(
+      $("html, body, .btn-liquid").animate(
         {
           scrollTop: target.offset().top - 60
         },
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10376,7 +10376,7 @@ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     var target = $(this.hash);
     target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
     if (target.length) {
-      $("html, body").animate({
+      $("html, body, .btn-liquid").animate({
         scrollTop: target.offset().top - 60
       }, 1000, "easeInOutExpo");
       return false;
