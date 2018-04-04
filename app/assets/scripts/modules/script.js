3be15ee47bed17d8c4ed01f2fa7ddb13e507b372
John Milner <john.milner@mac.com>
2018-04-03 20:52:58 -0700

    implement particle box position relative in footer

--- app/assets/scripts/modules/script.js
+++ app/assets/scripts/modules/script.js
@@ -464,7 +464,7 @@ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     var target = $(this.hash);
     target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
     if (target.length) {
-      $("html, body, .btn-liquid").animate(
+      $("html, body").animate(
         {
           scrollTop: target.offset().top - 60
         },
